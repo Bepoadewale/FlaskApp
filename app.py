a32d648c3758b5f01ee5660fbf194f93a78e1631
bepoadewale <bepoadewale@yahoo.com>
2024-12-23 14:45:27 +0100

    c29

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return jsonify({
-        "message": "Zayzoon rocking here!",
+        "message": "Adewale does little magic!",
         "timestamp": int(time.time())
     })
 
